d922b5448a67c3eae056516f19f2558bc71eeab0
Caus-tubh <kaustubhsingh717@gmail.com>
2024-05-03 01:38:26 +0530

    Added functionality of DC motor and encoder. Made code readable

--- main/main.ino
+++ main/main.ino
@@ -1,30 +1,33 @@
 // JC borewell device
 // GSM code
 // Node_ID = 2023;
-#define SERVER = "api.thingspeak.com";
-#define RESOURCE = "https://api.thingspeak.com/update?";
-#define API_KEY = "PI7HWMKGZH0RS83G";
-#define PORT = 80;
-#define APN = "cmnet";
 //const char gprsUser[] = "";
 //const char gprsPass[] = "";
 //const char simPIN[] = "";
-
-bool res = 0;
-bool wrong_distance = 0;
-long duration;
-int sent_status = 0;
-
-#define WDT_TIMEOUT 32400000
-#define WINDUP_PIN 13
-#define MODEM_RST 14
-#define MODEM_TX 26
-#define MODEM_RX 27
+#define SERVER "api.thingspeak.com";
+#define RESOURCE "https://api.thingspeak.com/update?";
+#define API_KEY "PI7HWMKGZH0RS83G";
+#define PORT 80;
+#define APN "cmnet";
 #define SerialMon Serial
 #define SerialAT Serial1
 #define TINY_GSM_MODEM_SIM800
-// Modem is SIM800
+#define WDT_TIMEOUT 32400000 // 9 hours
 #define TINY_GSM_RX_BUFFER 1024  // Set RX buffer to 1Kb
+
+#define MOTFWD  15 
+#define MOTREV  2
+#define ENCODERPINA 4; 
+#define ENCODERPINB 16;
+#define TENSION_SWITCH 17; 
+#define WINDUP_SWITCH 5
+#define MODEM_RST 0
+#define TX 18
+#define RX 19
+
+#include <SPI.h>
+#include <Wire.h>
+#include <esp_task_wdt.h>
 #include <Wire.h>
 #include <TinyGsmClient.h>
 #ifdef DUMP_AT_COMMANDS
@@ -36,65 +39,47 @@ TinyGsm modem(debugger);
 #else
 TinyGsm modem(SerialAT);
 #endif
-
 TinyGsmClient client(modem);
-#include <SPI.h>
-#include <Wire.h>
 
-int windup_reading = 0;
-int stepPin = 2;
-int dirPin = 0;
-int forcePin = 34;
-int reading = 0;
 
-float distance = 0;  // Stores the distance of the bob from the ground surface in cm
-int stepval = 50;    // We want 50 mm ticks
-int nsteps = floor(stepval / dist_per_step);
-
+bool windup_reading = 0;
+int tension_reading = 0;
+int last_encoded = 0; // Here updated value of encoder store.
+long encoder_value = 0; // Raw encoder value
+long starting_encoder_value = 0;
 unsigned long int reset_time = millis();
-#include <esp_task_wdt.h>
+float diameter = 10
+float circumference = 3.14159 * diameter 
+bool res = 0;
+bool wrong_distance = 0;
+long duration;
+bool sent_status = 0;
+
 
 void windUp()  // This function needs to be called only once in the void setup because it has to run only when the system is powered
 {
   do {
-    digitalWrite(dirPin, LOW);
-    for (int i = 0; i < nsteps; i++)  // On full stepping, 200 steps = 1 revolution
-    {
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(2500);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(2500);
-//      esp_task_wdt_reset();
-    }
-    windup_reading = (windup_reading + analogRead(WINDUP_PIN))/2;
-  } while(windup_reading < 3000);
+    digitalWrite(MOTFWD, LOW);  
+    digitalWrite(MOTREV, HIGH);
+    windup_reading = digitalRead(WINDUP_SWITCH);
+  } while(windup_reading == 1); 
+  digitalWrite(MOTREV, LOW);
   distance = 0;
+  starting_encoder_value = encoder_value;
 }
 
-void setup() {
-  // Declaring PinModes
-  Serial.begin(115200);
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(forcePin, INPUT);
-  delay(1000);
-  Serial.flush();
+void IRAM_ATTR updateEncoder(){
+  int MSB = digitalRead(ENCODERPINA); //MSB = most significant bit
+  int LSB = digitalRead(ENCODERPINB); //LSB = least significant bit
 
-  esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
-  esp_task_wdt_add(NULL);
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (last_encoded << 2) | encoded; //adding it to the previous encoded value
 
-  // GSM setup code
-  pinMode(MODEM_RST, OUTPUT);
-  digitalWrite(MODEM_RST, HIGH);
-  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-  delay(3000);
-  SerialMon.println("Initializing modem...");
-  modem.restart(); 
-  #if TINY_GSM_USE_GPRS
-  // Unlock your SIM card with a PIN if needed
-  if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
-  #endif
-  windUp();  // This will make sure that the bob is at the top before starting readings
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoder_value --;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoder_value ++;
+
+  last_encoded = encoded; //store this value for next time
+
 }
 
 void send_data(float Reading) {
@@ -130,64 +115,65 @@ void send_data(float Reading) {
   }
 }
 
+void setup() {
+  Serial.begin(115200);
+  SerialAT.begin(115200, SERIAL_8N1, RX, TX);
+  
+  pinMode(TENSION_SWITCH, INPUT_PULLUP);
+  pinMode(WINDUP_SWITCH, INPUT_PULLUP);
+  pinMode(MOTFWD, OUTPUT); 
+  pinMode(MOTREV, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(ENCODERPINA, INPUT_PULLUP); 
+  pinMode(ENCODERPINB, INPUT_PULLUP);
+  delay(1000);
+  Serial.flush();
+
+  esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL);
+  delay(3000);
+  
+  SerialMon.println("Initializing modem...");
+  digitalWrite(MODEM_RST, HIGH);
+  modem.restart(); 
+  #if TINY_GSM_USE_GPRS
+  // Unlock your SIM card with a PIN if needed
+  if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
+  #endif
+  
+  attachInterrupt(ENCODERPINA, updateEncoder, CHANGE); 
+  attachInterrupt(ENCODERPINB, updateEncoder, CHANGE);
+  
+  windUp();  // This will make sure that the bob is at the top before starting readings
+}
+
 void loop() {
     sent_status = 0;
     SerialMon.println("Starting the Collection of Data");
-    reading = analogRead(forcePin);
-    if (reading >= 2500) {
-      reading = 4095;
-    } else {
-      reading = 0;
+    tension_reading = digitalRead(TENSION_SWITCH);
+    while (tension_reading == 0){
+      digitalWrite(MOTFWD, LOW);  
+      digitalWrite(MOTREV, HIGH); 
+      delay(200);   
+      tension_reading = digitalRead(TENSION_SWITCH);
     }
-    stepval = 50;  // We want 50 mm ticks
-    int nsteps = floor(stepval / dist_per_step);
+    digitalWrite(MOTREV, LOW); 
+    distance -= (encoder_value - starting_encoder_value) * circumference;
+    Serial.print("Distance = ");
+    Serial.println((int)distance);
 
-    while (reading <= 10)  // This takes care of the case when the bob is on the water surface when the microcontroller wakes up
-    {
-      digitalWrite(dirPin, LOW);
-      for (int i = 0; i < nsteps; i++)  // On full stepping, 200 steps = 1 revolution
-      {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(2500);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(2500);
-//        esp_task_wdt_reset();
-      }
-      delay(200);  // This delay allows for the force sensor value to stabilize after the motor rotates
-      reading = analogRead(forcePin);
-      if (reading >= 2500) {
-        reading = 4095;
-      } else {
-        reading = 0;
-      }
-      distance -= stepval / 10;
-      Serial.print("Distance = ");
-      Serial.println((int)distance);
+    while (tension_reading == 1){
+      digitalWrite(MOTFWD, HIGH);  
+      digitalWrite(MOTREV, LOW); 
+      delay(200);   
+      tension_reading = digitalRead(TENSION_SWITCH);
     }
-
-    while (reading >= 10)  // This takes care of the case when the bob is hanging when the microcontroller wakes up
-    {
-      digitalWrite(dirPin, HIGH);
-      for (int i = 0; i < nsteps; i++)  // On full stepping, 200 steps = 1 revolution
-      {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(2500);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(2500);
-//        esp_task_wdt_reset();
-      }
-      delay(200);  // This delay allows for the force sensor value to stabilize after the motor rotates
-      reading = analogRead(forcePin);
-      if (reading >= 2500) {
-        reading = 4095;
-      } else {
-        reading = 0;
-      }
-      distance += stepval / 10;
-      Serial.print("Distance  = ");
-      Serial.println((int)reading);
-    }
+    digitalWrite(MOTFWD, LOW); 
+    distance += (encoder_value - starting_encoder_value) * circumference;
+    Serial.print("Distance = ");
+    Serial.println((int)distance);
     delay(500);
+    
     modem.gprsConnect(apn);
     modem.waitForNetwork(600000L);
     res = modem.isGprsConnected();
